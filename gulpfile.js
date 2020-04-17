b120f96b2e71dfbdee7db188fae747124d199a68
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-17 14:53:29 +0530

    configured gulp watch for changes in sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 const  gulp = require('gulp');
 const sass = require('gulp-sass');
 
+
+// // Gulp 3.x watch syntax
+// gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
+
+// // NOTE! Gulp 4.x watch syntax (all the rest of the examples would need to be like this for Gulp 4
+// gulp.watch('files-to-watch', gulp.series(['tasks', 'to', 'run']));
+
+//gulp.watch('app/scss/**/*.scss', gulp.series(['sass'])); 
+
+
+
+
+
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('destination'))
   });
+
+  gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
+    // Other watchers
+  })
