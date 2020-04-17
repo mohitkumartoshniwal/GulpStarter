863a0446a987a17b8945bd5cae00ac13c94f1f33
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-17 14:19:26 +0530

    configured gulp for any file ending with scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const  gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/styles.scss')
+    return gulp.src('app/scss/**/*.scss')
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('destination'))
   });
