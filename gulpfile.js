913b09e919e9bcdf6b3872559d6af5bfdb4f4fbb
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-17 16:47:22 +0530

    configured gulp watch for changes in sass files in gulp4 style

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const  gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 
 // // Gulp 3.x watch syntax
@@ -9,18 +10,66 @@ const sass = require('gulp-sass');
 // gulp.watch('files-to-watch', gulp.series(['tasks', 'to', 'run']));
 
 //gulp.watch('app/scss/**/*.scss', gulp.series(['sass'])); 
+var paths = {
+    styles: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "src/scss/*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "src/css"
+    }
+ 
+    // Easily add additional paths
+    // ,html: {
+    //  src: '...',
+    //  dest: '...'
+    // }
+};
 
 
 
-
-
-gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
+ function style(){
+    return gulp.src(paths.styles.src)
       .pipe(sass()) // Using gulp-sass
-      .pipe(gulp.dest('destination'))
-  });
+      .pipe(gulp.dest(paths.styles.dest))
+      .pipe(browserSync.stream());
+  }
 
-  gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
-    // Other watchers
-  })
+ function watch() {
+    browserSync.init({
+      server: {
+        baseDir: './src',
+        index: "/index.html"
+      },
+    }),
+    gulp.watch(paths.styles.src, style);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+}
+  
+
+//   gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'], function (){
+//     // ...
+//   })
+
+ // Don't forget to expose the task!
+exports.watch = watch
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.parallel(style, watch);
+ 
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+//gulp.task('build', build);
+ 
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task('default', build);
